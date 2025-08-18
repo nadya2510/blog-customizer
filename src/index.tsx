336fa22f8638cbe64148970737c15b39199c7ec0
Nadya Nikolaeva <nadya.nik.ven@yandex.ru>
2025-08-18 09:47:50 +0300

    commit3.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
+import { StrictMode} from 'react';
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { App } from './components/app/App'
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [styleArticle, setStyleArticle] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	return (
-		<main
-			className={styles.main}
-			style={
-				{
-					'--font-family': styleArticle.fontFamilyOption.value,
-					'--font-size': styleArticle.fontSizeOption.value,
-					'--font-color': styleArticle.fontColor.value,
-					'--container-width': styleArticle.contentWidth.value,
-					'--bg-color': styleArticle.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm onApply={setStyleArticle} />
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
